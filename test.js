6ace143d72364e25d4e886858a2a517b60410fc8
Vachell <vastiansen@gmail.com>
2022-11-26 23:08:01 +0700

    mantap

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-const input = "12/30/2022Z"
+const input = "12/30/2022"
+const inputdiedit = input + "Z"
 
 
-const tanggal = new Date(input)
+const tanggal = new Date(inputdiedit)
 
 const tanggalDoang = () => {
   return [tanggal.getDate(), tanggal.getMonth() + 1, tanggal.getFullYear()].join('-')
