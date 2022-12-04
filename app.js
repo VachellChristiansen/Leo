d37cce82c09b0d01a5a44288be62c2d4ebd09b10
Vachell <vastiansen@gmail.com>
2022-12-04 11:05:14 +0700

    get form data using express

--- app.js
+++ app.js
@@ -8,39 +8,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.render('cocokin')
 })
 
-app.post('/signup', async (req, res) => {
-  console.log(req.body)
-  const { data: signup, error: signupErr } = await supabase.auth.signUp({
-    email: req.body.email,
-    password: req.body.password
-  })
-  const { data: signupData, error: signupDataError } = await supabase
-    .from('profiles')
-    .insert([
-      { id: signup.user.id, name: req.body.name },
-    ])
-  console.log(signup, signupErr)
-  console.log(signupData, signupDataError)
-  res.redirect('/')
-})
-
-app.post('/signin', async (req, res) => {
-  console.log(req.body)
-  const { data: signin, error: signinErr } = await supabase.auth.signInWithPassword({
-    email: req.body.email,
-    password: req.body.password
-  })
-  console.log(signin, signinErr)
-  let { data: userData, error: userDataErr } = await supabase
-  .from('profiles')
-  .select('name')
-  .eq('id', signin.user.id)
-  console.log(userData)
-  res.render('data', {user: userData[0].name})
+app.get('/post', (req, res, next) => {
+  console.log(req.query);
 })
 
 app.listen(3000, () => {
